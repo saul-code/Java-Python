81c3aa6cfa343663fdff92e19f71f31be902b3a1
saul-code <80424689+saul-code@users.noreply.github.com>
2021-04-24 20:02:32 -0500

    PiedraPapelTijera script
    Los primeros cambios al script piedra papel tijera

---
+++ Java/PiedraPapelTijera.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class PiedraPapelTijera{
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+		Random aleatorios = new Random();
+
+		System.out.println(" Hola mucho gusto mi nombre es: \r\n Saul Lopez");
+		
+		
+		
+	}
+}
