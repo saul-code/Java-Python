c9316244a7319d232e6e0b6082fc6fdcf33745cc
saul-code <80424689+saul-code@users.noreply.github.com>
2021-04-25 02:42:26 -0500

    Version que funciona

--- Java/PiedraPapelTijera.java
+++ Java/PiedraPapelTijera.java
@@ -6,23 +6,30 @@ public class PiedraPapelTijera{
 	public static void main(String[] args) {
 
 		Scanner entrada = new Scanner(System.in);
-
-		//Incio del programa
-		System.out.println("\t--- Piedra, Papel o Tijera --- \n Escribe una opcion para jugar:\tPiedra, Papel o Tijera");
 		String opcionJugador = entrada.next();
 		String opcionMaquina = jugada();
+		String resultado = "";
+
+		//Incio del programa
+		System.out.println("\n\t--- Piedra, Papel o Tijera --- \n Escribe una opcion para jugar:\tPiedra, Papel o Tijera");
+		
 
 		//Condicionales
 		if(valido(opcionJugador)){
+			if( opcionJugador.toUpperCase().equals(opcionMaquina)){
 
-			
+				resultado = "\t Empate :/ \n";
+			} else {
 
+				 resultado = (jugar(opcionJugador,opcionMaquina))? "\t Ganaste :D\n":"\t Perdiste :C\n";
+			}
+
+			System.out.println(" La maquina eligio: " + opcionMaquina.toLowerCase());
+			System.out. println(resultado);
 		}else {
-			System.out.println("\tIngresa un valor valido :S");
+
+			System.out.println("\t Ingresa un valor valido :S \n");
 		}
-		
-
-
 	}
 
 	//metodos
@@ -37,16 +44,16 @@ public class PiedraPapelTijera{
 	}
 
 	//jugada maquina
-	public static String jugada(){
-		Random r = new Random();
-		int a = r.nextInt(3);
+	private static String jugada(){
+		Random aleatorio = new Random();
+		int a = aleatorio.nextInt(3);
 		String valor = "";
 
-		if(a == 1){
+		if(a == 0){
 			valor = "PIEDRA";
-		}else if(a == 2){
+		}else if(a == 1){
 			valor = "PAPEL";
-		} else if(a == 3){
+		} else if(a == 2){
 			valor = "TIJERA";
 		}
 		return valor;
@@ -54,7 +61,14 @@ public class PiedraPapelTijera{
 	}
 
 	//jugar
-	//public static boolean jugar(){}
+	public static boolean jugar(String var1, String var2){
+		String a = var1.toUpperCase();
+		if( (a.equals("PIEDRA") && var2.equals("TIJERA")) ||  (a.equals("PAPEL") && var2.equals("PIEDRA")) || (a.equals("TIJERA") && var2.equals("PAPEL")) ){
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 
 }
