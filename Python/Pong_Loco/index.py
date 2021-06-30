bdee9517d86c2a4e842c97a0fa71b05394fa47c3
Saul <esoterikouno@gmail.com>
2021-06-30 15:00:41 -0500

    Agregamos la CLase Pelota
    con sus atributos y agregamos el metodo mover pelota para que rebotara :

--- Python/Pong_Loco/index.py
+++ Python/Pong_Loco/index.py
@@ -6,23 +6,55 @@ WIDTH = 640
 HEIGHT = 480
 COLOR = (0,140,60)
 IMAGEN = pygame.image.load('imagenes/fondo.jpg')
+direccion = True
+#clases
+class Pelota(pygame.sprite.Sprite):
+    def __init__(self,color,radio,posicion=[WIDTH/2,HEIGHT/2],velocidad=[.5,-.5]):
+        pygame.sprite.Sprite.__init__(self)
+        self.color = color
+        self.radio = radio
+        self.velocidad = velocidad
+        self.posicion = posicion
+
+
+
+    def mover(self):
+        global direccion
+        if direccion == True:
+            if self.posicion[0] < 620:
+                self.posicion[0]+=self.velocidad[0]
+            else:
+                direccion = False
+        else:
+            if self.posicion[0] >20:
+                self.posicion[0]+=self.velocidad[1]
+            else:
+                direccion = True
+
 
 #Funciones
- 
+
 
 #main
 
 def main():
+    
     screen = pygame.display.set_mode((WIDTH,HEIGHT))
     pygame.display.set_caption("Prueba")
-    screen.blit(IMAGEN,(0,0))
+    pelota = Pelota((1,45,120),20)
+    #screen.blit(IMAGEN,(0,0))
+    
 
 
     while True:
-        #screen.fill(COLOR)
+        
         for events in pygame.event.get():
             if events.type == QUIT:
                 sys.exit(0)
+        screen.fill(COLOR) 
+        pygame.draw.circle(screen,pelota.color,pelota.posicion,pelota.radio)
+        pelota.mover()
+
         pygame.display.flip()
         #pygame.display.update()
         
